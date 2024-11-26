57e7dd3c347ea024e09748f4a6ff9879a4acb64f
Sam <samhooker@gmail.com>
2024-11-25 20:45:45 -0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    screens: {
+      sm: "375px",
+      md: "767px",
+      lg: "1400px",
+    },
+    extend: {
+      fontFamily: {
+        classic: ["Libre Baskerville", "sans-serif"],
+        fun: ["Parkinsans", "sans-serif"],
+      },
+      colors: {
+        rose500: "hsl(12, 20%, 44%)",
+        rose400: "hsl(7, 20%, 60%)",
+        rose300: "hsl(14, 25%, 72%)",
+      },
+    },
   },
 
   plugins: [],
